9e7e9aacfa08270383c57806dc270d743d8b8f16
Don Hardman <don.hardman@muvon.io>
2023-08-25 21:08:50 +0700

    Initial release of the MVP create table in sharded mode

---
+++ src/Handler.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2023, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+namespace Manticoresearch\Buddy\Plugin\CreateTable;
+
+use Manticoresearch\Buddy\Base\Sharding\Thread;
+use Manticoresearch\Buddy\Core\Plugin\BaseHandler;
+use Manticoresearch\Buddy\Core\Task\Task;
+use Manticoresearch\Buddy\Core\Task\TaskResult;
+use RuntimeException;
+use parallel\Runtime;
+
+final class Handler extends BaseHandler {
+	/**
+	 * Initialize the executor
+	 *
+	 * @param Payload $payload
+	 * @return void
+	 */
+	public function __construct(public Payload $payload) {
+	}
+
+  /**
+	 * Process the request
+	 *
+	 * @return Task
+	 * @throws RuntimeException
+	 */
+	public function run(Runtime $runtime): Task {
+		$args = $this->payload->toArgs();
+		Thread::instance()->execute('shard', $args);
+
+		// TODO: what we should do here?
+		$taskFn = static function (): TaskResult {
+			return TaskResult::none();
+		};
+
+		return Task::createInRuntime(
+			$runtime, $taskFn, []
+		)->run();
+	}
+
+	/**
+	 * @return array<string>
+	 */
+	public function getProps(): array {
+		return [];
+	}
+}
---
+++ src/Payload.php
@@ -0,0 +1,87 @@
+<?php declare(strict_types=1);
+
+/*
+  Copyright (c) 2023, Manticore Software LTD (https://manticoresearch.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License version 2 or any later
+  version. You should have received a copy of the GPL license along with this
+  program; if you did not, you can find it at http://www.gnu.org/
+*/
+namespace Manticoresearch\Buddy\Plugin\CreateTable;
+
+use Manticoresearch\Buddy\Core\Error\QueryParseError;
+use Manticoresearch\Buddy\Core\Network\Request;
+use Manticoresearch\Buddy\Core\Plugin\BasePayload;
+
+/**
+ * This is simple do nothing request that handle empty queries
+ * which can be as a result of only comments in it that we strip
+ */
+final class Payload extends BasePayload {
+	public readonly string $path;
+	public readonly string $cluster;
+	public readonly string $table;
+	public readonly string $structure;
+	public readonly string $extra;
+	public readonly int $shardCount;
+	public readonly int $replicationFactor;
+
+  /**
+	 * @param Request $request
+	 * @return static
+	 */
+	public static function fromRequest(Request $request): static {
+		$pattern = "/CREATE\s+TABLE\s+"
+	    . '(?:(?P<cluster>[^:\s]+):)?(?P<table>[^:\s]+)\s*'
+	    . '\((?P<structure>.+?)\)\s*' // This line is changed to match table structure
+	    . '(?:shards=(?P<shards>\d+)\s*)?'
+	    . '(?:rf=(?P<rf>\d+)\s*)?'
+	    . '(?P<extra>.*)/ius';
+
+		if (!preg_match($pattern, $request->payload, $matches)) {
+			QueryParseError::throw('Failed to parse query');
+		}
+		$self = new static();
+		// We just need to do something, but actually its' just for PHPstan
+		$self->path = $request->path;
+		$self->cluster = $matches['cluster'] ?? '';
+	  $self->table = $matches['table'];
+	  $self->structure = $matches['structure'];
+	  $self->shardCount = (int)($matches['shards'] ?? 2);
+	  $self->replicationFactor = (int)($matches['rf'] ?? 2);
+	  $self->extra = $matches['extra'];
+		return $self;
+	}
+
+	/**
+	 * @param Request $request
+	 * @return bool
+	 */
+	public static function hasMatch(Request $request): bool {
+		return stripos($request->payload, 'create table') === 0;
+	}
+
+	/**
+	 * Convert the current state into array
+	 * that we use for args in event
+	 * @return array{
+	 * cluster:string,
+	 * table:string,
+	 * structure:string,
+	 * extra:string,
+	 * replicationFactor:int,
+	 * shardCount:int
+	 * }
+	 */
+	public function toArgs(): array {
+		return [
+			'cluster' => $this->cluster,
+			'table' => $this->table,
+			'structure' => $this->structure,
+			'extra' => $this->extra,
+			'replicationFactor' => $this->replicationFactor,
+			'shardCount' => $this->shardCount,
+		];
+	}
+}
